dfb09fbbb2d24876d7ba76a2dc08cd1a8e9ed175
akbar <akbar@solusidata.co.id>
2021-12-14 19:18:09 +0700

    ok

--- app/Http/Middleware/InstallationMiddleware.php
+++ app/Http/Middleware/InstallationMiddleware.php
@@ -16,15 +16,15 @@ class InstallationMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if (! \Storage::disk('local')->has('database_created')) {
-            return redirect('/installation');
-        }
+        // if (! \Storage::disk('local')->has('database_created')) {
+        //     return redirect('/installation');
+        // }
 
-        if (\Storage::disk('local')->has('database_created')) {
-            if (Setting::getSetting('profile_complete') !== 'COMPLETED') {
-                return redirect('/installation');
-            }
-        }
+        // if (\Storage::disk('local')->has('database_created')) {
+        //     if (Setting::getSetting('profile_complete') !== 'COMPLETED') {
+        //         return redirect('/installation');
+        //     }
+        // }
 
         return $next($request);
     }
